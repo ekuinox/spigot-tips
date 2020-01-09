ee8d9d5e10ba2b31d3a7891be69e7023cbe0d540
ekuinox <depkey@me.com>
2020-01-09 15:49:04 +0900

    command executor

--- src/main/scala/dev/ekuinox/spigot_tips/CommandExecutor.scala
+++ src/main/scala/dev/ekuinox/spigot_tips/CommandExecutor.scala
@@ -4,6 +4,28 @@ import org.bukkit.command.{Command, CommandSender, CommandExecutor => BukkitComm
 
 class CommandExecutor extends BukkitCommandExecutor {
   override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
+    if (args == null) return true
+
+    args(0) match {
+      case "create" => create()
+      case "show" => show()
+      case "help" => help()
+      case _ => help()
+    }
+
     true
   }
+
+  def create(): Unit = {
+
+  }
+
+  def show(): Unit = {
+
+  }
+
+  def help(): Unit = {
+
+  }
+
 }
